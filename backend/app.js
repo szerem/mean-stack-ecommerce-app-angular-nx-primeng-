32999d9935be0a9af5187a209596230f34daf887
ks <ks@ks.ks>
2022-08-18 17:32:41 +0200

    init dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,15 @@
 
 const express = require("express");
 const app = express();
+require("dotenv/config");
+const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-    res.send('hello from API')
+// /api/v1
+app.get(api+"/products", (req, res) => {
+  res.send("hello from API");
 });
 
 app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+  console.log(api);
+  console.log(`server is running http://localhost:3000${api}`);
 });
