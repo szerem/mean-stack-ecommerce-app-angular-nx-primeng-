9dafba8dc6b7015b7f62d15a6079c7c1590ff5e7
ks <ks@ks.ks>
2022-08-18 17:22:34 +0200

    init get on express

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@
 
 const express = require("express");
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello from API')
+});
+
 app.listen(3000, () => {
   console.log("server is running http://localhost:3000");
 });
