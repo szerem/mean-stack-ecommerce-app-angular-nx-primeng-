e9d52746ac49835850e824ddb21a5b4b21eb25c1
ks <ks@ks.ks>
2022-08-18 17:20:16 +0200

    init express

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+// console.log("hello word !")
+
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+  console.log("server is running http://localhost:3000");
+});
