1fa474c1f9a3e78349803881ec9831fffa0b49f6
ks <ks@ks.ks>
2022-08-18 17:39:15 +0200

    api get products

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 // console.log("hello word !")
 
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv/config");
+require('dotenv/config');
 const api = process.env.API_URL;
 
 // /api/v1
-app.get(api+"/products", (req, res) => {
-  res.send("hello from API");
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresses',
+    image: 'some_url'
+  };
+  res.send(product);
 });
 
 app.listen(3000, () => {
-  console.log(api);
   console.log(`server is running http://localhost:3000${api}`);
 });
